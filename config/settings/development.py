1738069631db3e11f3ebccdaf41d9b1c1c01544f
Abdi Berhe <abdiberhe@gmail.com>
2024-04-08 17:12:16 +0300

    Docker confuguration

---
+++ config/settings/development.py
@@ -0,0 +1,17 @@
+from .base import *
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Development Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "../", "mediafiles")
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "../", "staticfiles")
---
+++ config/settings/production.py
@@ -0,0 +1,31 @@
+from .base import *
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "'smtp.mailgun.org'"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config("EMAIL_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
+EMAIL_USE_TLS = True
+
+
+# Production Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USERNAME"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOSTNAME"),
+        "PORT": config("DB_PORT", cast=int),
+    }
+}
+
+# Redis Cache
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": config("REDIS_BACKEND"),
+    },
+}
+
